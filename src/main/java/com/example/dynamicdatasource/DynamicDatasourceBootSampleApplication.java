52ef61dbba4a266fb5f36475e1c9fe36688ceaf7
Sunshow <sunshow@gmail.com>
2022-07-11 10:46:17 +0800

    simple start

---
+++ src/main/java/com/example/dynamicdatasource/DynamicDatasourceBootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDatasourceBootSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatasourceBootSampleApplication.class, args);
+    }
+
+}
